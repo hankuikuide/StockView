8893b697493f6307bf48d84625e82187f3d048ee
hankk <hankuikuide@163.com>
2018-06-08 15:35:05 +0800

    2
    2

---
+++ StockView/Stock.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockView
+{
+    public class Stock
+    {
+        public string Symbol { get; set; }
+        public string Name { get; set; }
+        public string Price { get; set; }
+        public string Increase { get; set; }
+        public string Open { get; set; }
+        public string PrevClose { get; set; }
+       
+
+
+    }
+}
